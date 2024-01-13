12f4636e3c2193df67bdb0a330ae77758cbf67e3
Yahya <yahyaoulha0@gmail.com>
2024-01-13 21:27:13 +0100

    worked

---
+++ models/engine/db_storage.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""This module defines a class to manage database storage for hbnb clone"""
+from models.base_model import Base
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+from os import getenv
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import (create_engine)
+
+
+classes = {'State': State, 'City': City,
+				'User': User, 'Place': Place,
+				'Review': Review, 'Amenity': Amenity}
+
+
+class DBStorage:
+	"""Connect and create tables"""
+	__engine = None
+	__session = None
+
+	def __init__(self):
+		user = getenv("HBNB_MYSQL_USER")
+		passwd = getenv("HBNB_MYSQL_PWD")
+		db = getenv("HBNB_MYSQL_DB")
+		host = getenv("HBNB_MYSQL_HOST")
+		env = getenv("HBNB_ENV")
+
+		self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+										.format(user, passwd, host, db),
+										pool_pre_ping=True)
+		if env == "test":
+			Base.metadata.drop_all(self.__engine)
+
+	def all(self, cls=None):
+		"""Returns a dictionary of __object__"""
+		NewDic = {}
+		if cls:
+			if type(cls) is str:
+				cls = eval(cls)
+			query = self.__session.query(cls)
+			for elem in query:
+				key = "{}.{}".format(type(elem).__name__, elem.id)
+				NewDic[key] = elem
+		else:
+			for key, val in classes.items():
+				for row in self.__session.query(val):
+					NewDic.update({'{}.{}'.
+									format(type(row).__name__, row.id,): row})
+		return NewDic
+
+	def new(self, obj):
+		"""Add an object to the current database"""
+		self.__session.add(obj)
+
+	def save(self):
+		"""Commit the changes to the database"""
+		self.__session.commit()
+
+	def delete(self, obj=None):
+		"""delete an element in the table"""
+		if obj:
+			self.session.delete(obj)
+
+	def reload(self):
+		"""Create a database session"""
+		Base.metadata.create_all(self.__engine)
+		session = sessionmaker(bind=self.__engine,
+								expire_on_commit=False)
+		self.__session = scoped_session(session)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import models
+from models.city import City
+import shlex
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(120), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
+    @property
+    def cities(self):
+        var = models.storage.all()
+        CityList = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                CityList.append(var[key])
+        for elem in CityList:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
