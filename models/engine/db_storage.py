fc0d72c974b823bf546b791d25a15f4dea4538ce
Yahya <yahyaoulha0@gmail.com>
2024-01-13 22:00:20 +0100

    worked

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -13,61 +13,61 @@ from sqlalchemy import (create_engine)
 
 
 classes = {'State': State, 'City': City,
-				'User': User, 'Place': Place,
-				'Review': Review, 'Amenity': Amenity}
+           'User': User, 'Place': Place,
+           'Review': Review, 'Amenity': Amenity}
 
 
 class DBStorage:
-	"""Connect and create tables"""
-	__engine = None
-	__session = None
+    """Connect and create tables"""
+    __engine = None
+    __session = None
 
-	def __init__(self):
-		user = getenv("HBNB_MYSQL_USER")
-		passwd = getenv("HBNB_MYSQL_PWD")
-		db = getenv("HBNB_MYSQL_DB")
-		host = getenv("HBNB_MYSQL_HOST")
-		env = getenv("HBNB_ENV")
+    def __init__(self):
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
 
-		self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-										.format(user, passwd, host, db),
-										pool_pre_ping=True)
-		if env == "test":
-			Base.metadata.drop_all(self.__engine)
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, passwd, host, db),
+                                      pool_pre_ping=True)
+        if env == "test":
+            Base.metadata.drop_all(self.__engine)
 
-	def all(self, cls=None):
-		"""Returns a dictionary of __object__"""
-		NewDic = {}
-		if cls:
-			if type(cls) is str:
-				cls = eval(cls)
-			query = self.__session.query(cls)
-			for elem in query:
-				key = "{}.{}".format(type(elem).__name__, elem.id)
-				NewDic[key] = elem
-		else:
-			for key, val in classes.items():
-				for row in self.__session.query(val):
-					NewDic.update({'{}.{}'.
-									format(type(row).__name__, row.id,): row})
-		return NewDic
+    def all(self, cls=None):
+        """Returns a dictionary of __object__"""
+        NewDic = {}
+        if cls:
+            if type(cls) is str:
+                cls = eval(cls)
+            query = self.__session.query(cls)
+            for elem in query:
+                key = "{}.{}".format(type(elem).__name__, elem.id)
+                NewDic[key] = elem
+        else:
+            for key, val in classes.items():
+                for row in self.__session.query(val):
+                    NewDic.update({'{}.{}'.
+                                  format(type(row).__name__, row.id,): row})
+        return NewDic
 
-	def new(self, obj):
-		"""Add an object to the current database"""
-		self.__session.add(obj)
+    def new(self, obj):
+        """Add an object to the current database"""
+        self.__session.add(obj)
 
-	def save(self):
-		"""Commit the changes to the database"""
-		self.__session.commit()
+    def save(self):
+        """Commit the changes to the database"""
+        self.__session.commit()
 
-	def delete(self, obj=None):
-		"""delete an element in the table"""
-		if obj:
-			self.session.delete(obj)
+    def delete(self, obj=None):
+        """delete an element in the table"""
+        if obj:
+            self.session.delete(obj)
 
-	def reload(self):
-		"""Create a database session"""
-		Base.metadata.create_all(self.__engine)
-		session = sessionmaker(bind=self.__engine,
-								expire_on_commit=False)
-		self.__session = scoped_session(session)
+    def reload(self):
+        """Create a database session"""
+        Base.metadata.create_all(self.__engine)
+        session = sessionmaker(bind=self.__engine,
+                               expire_on_commit=False)
+        self.__session = scoped_session(session)
