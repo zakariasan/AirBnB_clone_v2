c7e70315f59c324bd089341709850327a1abf37a
Yahya <yahyaoulha0@gmail.com>
2024-01-14 22:56:55 +0100

    hope it work

--- models/user.py
+++ models/user.py
@@ -6,6 +6,8 @@ from sqlalchemy import Column, String
 
 class User(BaseModel, Base):
     """This class defines a user and it's attributes"""
+    __tablename__ = "users"
+
     email = email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
