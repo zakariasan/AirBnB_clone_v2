7ee50bafa9472c5ef17782c8f9a94f32404709ae
Yahya <yahyaoulha0@gmail.com>
2024-01-15 14:33:09 +0100

    fixed

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
--- Create dev database and user
+-- Create dev database and user and grant it permissions
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,6 @@
- -- create a testing database for the AirBnB project
-
+-- Create test database and user and grant it permissions
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
-
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
