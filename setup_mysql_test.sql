2b9d1d885dc143432b7c127d5a8eeb2219fa4d67
zakariasan <elitziko@gmail.com>
2024-01-17 18:02:26 +0100

    setup_sql_test file

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,5 @@
 -- Create test database and user and grant it permissions
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
