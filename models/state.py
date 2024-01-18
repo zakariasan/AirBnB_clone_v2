d2635a699f65fdc7f800d748e936fa35ad67edce
Yahya <yahyaoulha0@gmail.com>
2024-01-18 12:52:16 +0100

    hope it works

--- models/state.py
+++ models/state.py
@@ -4,24 +4,27 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import models
-from os import environ
+import shlex
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(120), nullable=False)
-    if environ['HBNB_TYPE_STORAGE'] == 'db':
-        cities = relationship('City', cascade='all, delete', backref='state')
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
     @property
     def cities(self):
-        from models import storage
-        from models.city import City
         CityDict = models.storage.all()
         CityList = []
-        for city in CityDict.values():
-            if city.state_id == self.id:
-                CityList.append(city)
-
-        return CityList
+        result = []
+        for key in CityDict:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                CityList.append(CityDict[key])
+        for elem in CityList:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
