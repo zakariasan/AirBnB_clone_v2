1a1c09f120d15d676d1e1084a07a727624ba700a
Yahya <yahyaoulha0@gmail.com>
2024-01-18 12:38:38 +0100

    worked

--- models/state.py
+++ models/state.py
@@ -1,42 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import models
-from models.city import City
-import shlex
+from os import environ
 
 
 class State(BaseModel, Base):
     """ State class """
-    if models.env_type == "db":
-        __tablename__ = "states"
-        name = Column(String(120), nullable=False)
-        cities = relationship(
-                "City",
-                cascade='all, delete, delete-orphan',
-                backref="state")
-    else:
-        name = ''
+    __tablename__ = "states"
+    name = Column(String(120), nullable=False)
+    if environ['HBNB_TYPE_STORAGE'] == 'db':
+        cities = relationship('City', cascade='all, delete', backref='state')
 
-    if models.env_type != "db":
-        @property
-        def cities(self):
-            """for FileStorage: getter attribute cities"""
-            var = models.storage.all()
-            CityList = []
-            result = []
-            for key in var:
-                city = key.replace('.', ' ')
-                city = shlex.split(city)
-                if (city[0] == 'City'):
-                    CityList.append(var[key])
-            for elem in CityList:
-                if (elem.state_id == self.id):
-                    result.append(elem)
-            return (result)
+    @property
+    def cities(self):
+        from models import storage
+        from models.city import City
+        CityDict = models.storage.all()
+        CityList = []
+        for city in CityDict.values():
+            if city.state_id == self.id:
+                CityList.append(city)
 
-    def __init__(self, *args, **kwargs):
-        """State inherits from BaseModel and Base"""
-        super().__init__(*args, **kwargs)
+        return CityList
