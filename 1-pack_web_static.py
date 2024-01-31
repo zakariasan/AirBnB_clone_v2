33fb6d8c5c9046761c0f2bc46fad1b1ef35742c0
Yahya <yahyaoulha0@gmail.com>
2024-01-31 17:26:31 +0100

    worked

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" compress a folder and name it the  current time and date """
+from datetime import datetime
+from fabric.api import local
+import os
+
+
+def do_pack():
+    try:
+        if not os.path.exists("versions"):
+            local('mkdir versions')
+        CurrentDateTime = datetime.now().strftime("%Y%m%d%H%M%S")
+        ArchivePath = 'versions/web_static_{}.tgz'.format(CurrentDateTime)
+        local('tar -cvzf {} web_static'.format(ArchivePath))
+        return ArchivePath
+    except Exception as e:
+        return None
