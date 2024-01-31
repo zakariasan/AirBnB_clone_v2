1f65880d094d445bad2767143cda22ee8dc1ef72
Yahya <yahyaoulha0@gmail.com>
2024-01-31 17:57:04 +0100

    worked

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,9 +6,13 @@ import os
 
 
 def do_pack():
+    """ Pack Folder """
+    try:
         if not os.path.exists("versions"):
             local('mkdir versions')
         CurrentDateTime = datetime.now().strftime("%Y%m%d%H%M%S")
         ArchivePath = 'versions/web_static_{}.tgz'.format(CurrentDateTime)
         local('tar -cvzf {} web_static'.format(ArchivePath))
         return ArchivePath
+    except Exception:
+        return None
