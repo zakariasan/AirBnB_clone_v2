58ed2bb754bea242dc9cb23879475bff2e727d54
zakariasan <elitziko@gmail.com>
2024-02-16 17:19:21 +0100

    fun display fun routes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,5 +10,10 @@ def hello_hbnb():
     return 'Hello HBNB!'
 
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
