bb65855e44f71f4682ab974a18eddb57b416fc4a
zakariasan <elitziko@gmail.com>
2024-02-19 00:53:05 +0100

    nbrs and states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,24 +3,22 @@
 
 from flask import Flask, render_template
 from models import storage
-from models.state import State
-
+from models import *
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def teardown(exception):
-    """Teardown method to remove the current SQLAlchemy Session"""
-    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Route to display a list of states"""
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    """display page of listes with the states """
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
